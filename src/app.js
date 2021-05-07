fa4a369355169ec552e5bf6fa3b2c772e9f2ff36
eadogan <e.ahmetdogan@gmail.com>
2021-05-07 17:14:08 +0100

    Refactor chat app

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 const express = require('express')
-require('mongoose')
+const path = require('path')
+
+const publicDirectoryPath = path.join(__dirname, '../public')
 
 const app = express()
 
+
 app.use(express.json())
+app.use(express.static(publicDirectoryPath))
 
 module.exports = app
--- src/index.js
+++ src/index.js
@@ -1,6 +1,39 @@
 const app = require('./app')
+const http = require('http')
+const socketio = require('socket.io')
+const Filter = require('bad-words')
 const port = process.env.PORT
 
-app.listen(port, () => {
+const server = http.createServer(app)
+const io = socketio(server)
+
+io.on('connection', (socket) => {
+    console.log('New WebSocket connection')
+
+    socket.emit('message', 'Welcome!')
+    socket.broadcast.emit('message', 'A new user has joined!')
+
+    socket.on('sendMessage', (msg, callback) => {
+        const filter = new Filter()
+
+        if(filter.isProfane(msg)) {
+            return callback('Profanity is not allowed!')
+        }
+
+        io.emit('message', msg)
+        callback()
+    })
+
+    socket.on('sendLocation', (coords, callback) => {
+        io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+        callback()
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left!')
+    })
+})
+
+server.listen(port, () => {
     console.log(`Server is on port ${port}`)
 })
